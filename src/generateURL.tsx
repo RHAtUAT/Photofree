7c185891bceb238db146722ba9bc1be8dcb59ed8
RHAtUAT <39317306+RHAtUAT@users.noreply.github.com>
2023-02-28 19:19:01 -0700

    [WIP] Use correct key for generating presignedUrl

--- src/generateURL.tsx
+++ src/generateURL.tsx
@@ -4,28 +4,43 @@ import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 // Function to generate presigned URL
-export async function generatePresignedUrl(key: string): Promise<string> {
+export async function generatePresignedUrl(privateUrl: string): Promise<string> {
 
     // Retrieve the user's credentials
     const credentials = await Auth.currentCredentials();
     const client = new S3Client({
         region: awsmobile.aws_project_region,
-        credentials,
+        credentials: credentials
       });
+
+      console.log("Url:", privateUrl);
+
+    // Get the file "folder" (S3 doesn't actually contain folders https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-folders.html)
+    // Also don't know why the key doesn't have a prefix as the docs says it should.
+    const start = privateUrl.indexOf(".com") + 5; // + 5 excludes "".com/"", so elements are added after
+    const end = privateUrl.indexOf("?");
+    let filePath: string;
+    if (start > -1 && end > -1) {
+    filePath = privateUrl.substring(start, end);
+    console.log(filePath);
+    } else {
+    throw new Error("URL is invalid");
+    }
+      
     try {
         // Create an S3 request object
         const command = new GetObjectCommand({
             Bucket: awsmobile.aws_user_files_s3_bucket,
-            Key: key,
+            Key: filePath
         });
 
-        console.log("Key:", key);
+        console.log("Key:", filePath);
         console.log("Region:", awsmobile.aws_project_region);
         console.log("Credentials:", credentials);
 
         // Generate a presigned URL for the S3 object
         const signedUrl = await getSignedUrl(client, command, {
-            expiresIn: 3600,
+            expiresIn: 39600,
             signingRegion: awsmobile.aws_project_region,
         });
         console.log("URL:", signedUrl);
@@ -36,4 +51,3 @@ export async function generatePresignedUrl(key: string): Promise<string> {
         throw error;
     }
 }
-
